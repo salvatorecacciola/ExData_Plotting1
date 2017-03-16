5a5b1f8e10c28d29d56948927c57dc7a744f4608
salvatorecacciola <cacciola.sal@gmail.com>
2017-03-16 22:37:40 +0000

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,36 @@
+# load data and read
+library(data.table)
+library(lubridate)
+URL<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata
+%2Fhousehold_power_consumption.zip"
+download.file(URL, destfile="Electric.zip")
+unzip("Electric.zip", exdir="./Electric")
+setwd("./Electric")
+AllData<-fread("household_power_consumption.txt", sep=";", header=TRUE)
+setwd("..")
+
+#add date-time column
+
+AllData$DateTime<-dmy_hms(paste(AllData$Date, AllData$Time))
+AllData$Date<-dmy(AllData$Date)
+
+#filter
+
+AllData<-subset(AllData, Date>=as.Date("2007-02-01") 
+                & Date<=as.Date("2007-02-02"))
+
+#change classes to numeric
+AllData$Global_active_power<-as.numeric(AllData$Global_active_power)
+AllData$Global_reactive_power<-as.numeric(AllData$Global_reactive_power)
+AllData$Voltage<-as.numeric(AllData$Voltage)
+AllData$Global_intensity<-as.numeric(AllData$Global_intensity)
+AllData$Sub_metering_1<-as.numeric(AllData$Sub_metering_1)
+AllData$Sub_metering_2<-as.numeric(AllData$Sub_metering_2)
+AllData$Sub_metering_3<-as.numeric(AllData$Sub_metering_3)
+
+#Make plot
+png(file="plot2.png")
+with(data=AllData, plot(Global_active_power~DateTime, 
+                        type="l", xlab="", 
+                        ylab="Global Active Power (kilowatts)"))
+dev.off()
